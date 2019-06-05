ec1f270a944a09263b042d670ba55c9d4e5f1e93
Brook Jordan <brkjrdn@gmail.com>
2019-06-05 23:13:28 +0800

    Make color-function variables updatable

--- projects/polyfill-color-function/entry.js
+++ projects/polyfill-color-function/entry.js
@@ -1,1 +1,23 @@
 CSS.paintWorklet.addModule('./color-function-worklet.js');
+
+window.onload = e => {
+  let mainCircle = document.querySelector('div');
+
+  let ambientLightColorInput = document.createElement('input');
+  ambientLightColorInput.type = 'color';
+  ambientLightColorInput.onchange = e => {
+    mainCircle.style.setProperty('--ambient-light', ambientLightColorInput.value);
+  }
+
+  ambientLightColorInput.value = getComputedStyle(mainCircle).getPropertyValue('--ambient-light').trim();
+  document.body.prepend(ambientLightColorInput);
+
+  let directLightColorInput = document.createElement('input');
+  directLightColorInput.type = 'color';
+  directLightColorInput.onchange = e => {
+    mainCircle.style.setProperty('--direct-light', directLightColorInput.value);
+  }
+
+  directLightColorInput.value = getComputedStyle(mainCircle).getPropertyValue('--direct-light').trim();
+  document.body.prepend(directLightColorInput);
+}
