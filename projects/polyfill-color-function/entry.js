118656146f646c2cb6ac9d9794cf7e7baf8d9dde
Brook Jordan <brkjrdn@gmail.com>
2019-06-06 11:54:44 +0800

    Set background

--- projects/polyfill-color-function/entry.js
+++ projects/polyfill-color-function/entry.js
@@ -1,23 +1,23 @@
 CSS.paintWorklet.addModule('./color-function-worklet.js');
 
 window.onload = e => {
-  let mainCircle = document.querySelector('div');
+  let root = document.documentElement;
 
   let ambientLightColorInput = document.createElement('input');
   ambientLightColorInput.type = 'color';
   ambientLightColorInput.onchange = e => {
-    mainCircle.style.setProperty('--ambient-light', ambientLightColorInput.value);
+    root.style.setProperty('--ambient-light', ambientLightColorInput.value);
   }
 
-  ambientLightColorInput.value = getComputedStyle(mainCircle).getPropertyValue('--ambient-light').trim();
+  ambientLightColorInput.value = getComputedStyle(root).getPropertyValue('--ambient-light').trim();
   document.body.prepend(ambientLightColorInput);
 
   let directLightColorInput = document.createElement('input');
   directLightColorInput.type = 'color';
   directLightColorInput.onchange = e => {
-    mainCircle.style.setProperty('--direct-light', directLightColorInput.value);
+    root.style.setProperty('--direct-light', directLightColorInput.value);
   }
 
-  directLightColorInput.value = getComputedStyle(mainCircle).getPropertyValue('--direct-light').trim();
+  directLightColorInput.value = getComputedStyle(root).getPropertyValue('--direct-light').trim();
   document.body.prepend(directLightColorInput);
 }
