258b46c9a23be8b1f57652d97092e471681970cf
Brook Jordan <brkjrdn@gmail.com>
2020-02-23 11:23:42 +0800

    web manifest

--- projects/rumours/pwa--cache-details.js
+++ projects/rumours/pwa--cache-details.js
@@ -3,7 +3,7 @@ const cacheName = 'placeholder-v1';
 const staticAssets = [
   './',
   './index.html',
-  './main.1.0.2.css',
+  './main.1.0.3.css',
   'https://fonts.googleapis.com/css?family=Coming+Soon&display=swap',
 
   './favicon.ico',
@@ -14,7 +14,7 @@ const staticAssets = [
   './main.js',
   './words--3.2.1.js',
   './verbs--2.2.1.js',
-  './people--2.2.1.js',
+  './people--3.0.0.js',
   './settings--2.0.0.js',
   './query-params--1.0.0.js',
   './extra-people--1.1.0.js',
