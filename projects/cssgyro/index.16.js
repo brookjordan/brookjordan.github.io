b781cdf6012d3033e097614079559be256240450
Brook Jordan <brkjrdn@gmail.com>
2020-03-08 01:23:42 +0800

    DRY the iOS code

--- projects/cssgyro/index.16.js
+++ projects/cssgyro/index.16.js
@@ -1,5 +1,7 @@
-let orientationEventWorking = false;
-let motionEventWorking = false;
+const workingEventTypes = {
+  DeviceOrientationEvent: false,
+  DeviceMotionEvent: false,
+};
 
 function setGolbalCSSNumber(name, value) {
   document.documentElement.style.setProperty(
@@ -8,18 +10,23 @@ function setGolbalCSSNumber(name, value) {
   );
 }
 
-function handleOrientationChange(event) {event || (event = {});
+function handleOrientationChange(event) {
+  event || (event = {});
+
   setGolbalCSSNumber(
     "rotation--angle",
     (
       (screen && screen.orientation)
-        ? screen.orientation.angle
-        : window.orientation
+        ? screen.orientation.angle // not iOS
+        : window.orientation // iOS
     )
   );
 }
 
-function handleDeviceOrientation(event) {event || (event = {});
+function handleDeviceOrientation(event) {
+  event || (event = {});
+  workingEventTypes.DeviceOrientationEvent = true;
+
   setGolbalCSSNumber("gyro--absolute", event.absolute);
   setGolbalCSSNumber("gyro--z", event.alpha);
   setGolbalCSSNumber("gyro--x", event.beta);
@@ -27,11 +34,12 @@ function handleDeviceOrientation(event) {event || (event = {});
 
   setGolbalCSSNumber("compass--heading", event.compassHeading || event.webkitCompassHeading);
   setGolbalCSSNumber("compass--accuracy", event.compassAccuracy || event.webkitCompassAccuracy);
-
-  orientationEventWorking = true;
 }
 
-function handleDeviceMotion(event) {event || (event = {});
+function handleDeviceMotion(event) {
+  event || (event = {});
+  workingEventTypes.DeviceMotionEvent = true;
+
   setGolbalCSSNumber("motion--acceleration-x", (event.acceleration && event.acceleration.x) * 20);
   setGolbalCSSNumber("motion--acceleration-y", (event.acceleration && event.acceleration.y) * 20);
   setGolbalCSSNumber("motion--acceleration-z", (event.acceleration && event.acceleration.z) * 20);
@@ -45,64 +53,44 @@ function handleDeviceMotion(event) {event || (event = {});
   setGolbalCSSNumber("motion--rotation-rate-y", (event.rotationRate && event.rotationRate.gamma));
 
   setGolbalCSSNumber("motion--interval", event.interval * 20);
-
-  motionEventWorking = true;
 }
 
-function addOrientationEvent() {
-  if (orientationEventWorking) { return; }
+function tryToAddOrientationEventListener() {
+  if (workingEventTypes.DeviceOrientationEvent) { return; }
+
   window.addEventListener("deviceorientation", handleDeviceOrientation);
 }
 
-function addMotionEvent() {
-  if (motionEventWorking) { return; }
+function tryToAddMotionEventListener() {
+  if (workingEventTypes.DeviceMotionEvent) { return; }
+
   window.addEventListener("devicemotion", handleDeviceMotion);
 }
 
-function prepareOrientationAccessRequest() {
-  document.body.addEventListener("touchend", () => {
-    if (orientationEventWorking) { return; }
+function prepareMotionAccessRequest(type) {
+  let EventClass = window[type];
+  if (!EventClass || typeof EventClass.requestPermission !== "function") { return; }
 
-    DeviceOrientationEvent.requestPermission()
-    .then(permission => {
+  document.body.addEventListener("touchend", async () => {
+    if (workingEventTypes[type]) { return; }
+
+    try {
+      let permission = EventClass.requestPermission();
       if (permission === "granted") {
-        addOrientationEvent();
+        tryToAddOrientationEventListener();
       } else {
         document.body.innerHTML = "<h1>Sorry, you denied required permissions, this won’t work. ☹️</h1>";
       }
-    })
-    .catch(error => {
+    } catch (error) {
       prompt("Something went wrong with permissions", error);
-    });
-  }, { once: true });
-}
-
-function prepareMotionAccessRequest() {
-  document.body.addEventListener("touchend", () => {
-    if (motionEventWorking) { return; }
-
-    DeviceMotionEvent.requestPermission()
-    .then(permission => {
-      if (permission === "granted") {
-        addMotionEvent();
-      } else {
-        document.body.innerHTML = "<h1>Sorry, you denied required permissions, this won’t work. ☹️</h1>";
-      }
-    })
-    .catch(error => {
-      prompt("Something went wrong with permissions", error);
-    });
+    }
   }, { once: true });
 }
 
 window.addEventListener("orientationchange", handleOrientationChange);
 
-addOrientationEvent();
-if (window.DeviceOrientationEvent && typeof DeviceOrientationEvent.requestPermission === "function") {
-  prepareOrientationAccessRequest();
-}
+tryToAddOrientationEventListener();
+prepareMotionAccessRequest("DeviceOrientationEvent");
 
-addMotionEvent();
-if (window.DeviceMotionEvent && typeof DeviceMotionEvent.requestPermission === "function") {
-  prepareMotionAccessRequest();
-}
+tryToAddMotionEventListener();
+prepareMotionAccessRequest("DeviceMotionEvent");
