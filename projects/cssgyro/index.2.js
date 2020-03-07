5044127ab9f1ae217d2cdf4f1c9c6c561ff7b5a7
Brook Jordan <brkjrdn@gmail.com>
2020-03-07 23:04:42 +0800

    Use safari syntax

--- projects/cssgyro/index.2.js
+++ projects/cssgyro/index.2.js
@@ -7,7 +7,7 @@
     }
 
     function handleOrientationChange(event = {}) {
-      setGolbalCSSNumber("rotation--angle", screen.orientation.angle);
+      setGolbalCSSNumber("rotation--angle", (screen.orientation && screen.orientation.angle));
     }
 
     if (!window.DeviceOrientationEvent) {
@@ -27,17 +27,17 @@
       //alert("Your browser doesn’t support motion events");
     }
     function handleDeviceMotion(event = {}) {
-      setGolbalCSSNumber("motion--acceleration-x", event.acceleration?.x * 20);
-      setGolbalCSSNumber("motion--acceleration-y", event.acceleration?.y * 20);
-      setGolbalCSSNumber("motion--acceleration-z", event.acceleration?.z * 20);
+      setGolbalCSSNumber("motion--acceleration-x", (event.acceleration && event.acceleration.x) * 20);
+      setGolbalCSSNumber("motion--acceleration-y", (event.acceleration && event.acceleration.y) * 20);
+      setGolbalCSSNumber("motion--acceleration-z", (event.acceleration && event.acceleration.z) * 20);
 
-      setGolbalCSSNumber("motion--acceleration-including-gravity-x", event.accelerationIncludingGravity?.x * 20);
-      setGolbalCSSNumber("motion--acceleration-including-gravity-y", event.accelerationIncludingGravity?.y * 20);
-      setGolbalCSSNumber("motion--acceleration-including-gravity-z", event.accelerationIncludingGravity?.z * 20);
+      setGolbalCSSNumber("motion--acceleration-including-gravity-x", (event.accelerationIncludingGravity && event.accelerationIncludingGravity.x) * 20);
+      setGolbalCSSNumber("motion--acceleration-including-gravity-y", (event.accelerationIncludingGravity && event.accelerationIncludingGravity.y) * 20);
+      setGolbalCSSNumber("motion--acceleration-including-gravity-z", (event.accelerationIncludingGravity && event.accelerationIncludingGravity.z) * 20);
 
-      setGolbalCSSNumber("motion--rotation-rate-z", event.rotationRate?.alpha);
-      setGolbalCSSNumber("motion--rotation-rate-x", event.rotationRate?.beta);
-      setGolbalCSSNumber("motion--rotation-rate-y", event.rotationRate?.gamma);
+      setGolbalCSSNumber("motion--rotation-rate-z", (event.rotationRate && event.rotationRate.alpha));
+      setGolbalCSSNumber("motion--rotation-rate-x", (event.rotationRate && event.rotationRate.beta));
+      setGolbalCSSNumber("motion--rotation-rate-y", (event.rotationRate && event.rotationRate.gamma));
 
       setGolbalCSSNumber("motion--interval", event.interval * 20);
     }
