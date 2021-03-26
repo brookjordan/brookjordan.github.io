915182e187b6cdcf34170df288a81f01877f2a71
Brook Jordan <brook@brook.dev>
2021-03-26 15:18:06 +0800

    Show hex on lch

--- projects/lch/main.1.js
+++ projects/lch/main.1.js
@@ -1,6 +1,4 @@
-import * as colors from "./colors.1.js";
-
-let raf;
+let renderRAF;
 let lastFrameDrawnAt = 0;
 
 quality.max = Math.ceil(Math.min(Math.round(innerWidth / 2), Math.round(innerHeight / 2), 500) / 25) * 25;
@@ -13,34 +11,50 @@ let clipOpacity = +clip_opacity.value;
 let MAX_CHROMA = +max_chroma.value;
 let ctx = canvas.getContext("2d");
 
+let chroma = 0;
+let hue = 0;
+
 function drawBlock(color, x, y) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, 1, 1);
 }
 
-function shg(x, y) {
-  return Math.atan2(x, y) / Math.PI * 180;
-}
-
 function xyAngle(x, y) {
-  return Math.atan2(x, y) / Math.PI * 180;
+  return (Math.atan2(x, y) / Math.PI * 180 + 360) % 360;
 }
 
-function drawCross(lightness) {
-  cancelAnimationFrame(raf);
+function render(lightness) {
+  console.log(lightness);
+  cancelAnimationFrame(renderRAF);
 
-  function drawRow(y) {
+  function renderRow(y) {
     ctx.clearRect(0, y, size, 1);
     for (let x = 0; x < size; x += 1) {
       let xx = x - thickness;
       let yy = y - thickness;
       let angle = xyAngle(xx, yy);
 
-      let color = colors.LCH_to_sRGB_string([
+      let lch = [
         lightness,
         (MAX_CHROMA / (thickness + 1)) * (Math.sqrt(xx * xx + yy * yy)),
-        angle
-      ], clipOpacity);
+        angle,
+      ];
+
+      let color;
+      let colorObject = new Color({color: lch, type: 'lchab'});
+      let rgb = colorObject.rgb;
+      let rgbClipped = rgb.map(c => Math.max(0, Math.min(255, c)));
+
+      if(
+        rgb[0] >= 0 && rgb[0] <= 255
+        && rgb[1] >= 0 && rgb[1] <= 255
+        && rgb[2] >= 0 && rgb[2] <= 255
+      ) {
+        color = `rgb(${rgbClipped})`;
+      } else if (clipOpacity > 0) {
+        color = `rgba(${rgbClipped},${clipOpacity / 100})`;
+      }
+
       if (color) {
         drawBlock(
           color,
@@ -54,35 +68,38 @@ function drawCross(lightness) {
       let timeNow = new Date().getTime();
       if (lastFrameDrawnAt < timeNow - 1000 / 15) {
         lastFrameDrawnAt = timeNow;
-        raf = requestAnimationFrame(() => {
-          drawRow(y + 1);
+        renderRAF = requestAnimationFrame(() => {
+          renderRow(y + 1);
         });
       } else {
-        drawRow(y + 1);
+        renderRow(y + 1);
       }
     }
   }
 
-  raf = requestAnimationFrame(() => {
-    drawRow(0);
+  renderRAF = requestAnimationFrame(() => {
+    renderRow(0);
   });
 }
 
-drawCross(+range.value);
-range.oninput = e => drawCross(+range.value);
+render(+range.value);
+range.oninput = e => {
+  find_hex.value = new Color({ color: [+range.value, chroma, hue], type: 'lchab' }).hex;
+  render(+range.value)
+};
 max_chroma.oninput = e => {
   MAX_CHROMA = +max_chroma.value;
-  drawCross(+range.value)
+  render(+range.value)
 };
 clip_opacity.oninput = e => {
   clipOpacity = +clip_opacity.value;
-  drawCross(+range.value)
+  render(+range.value)
 };
 quality.oninput = e => {
   thickness = +quality.value;
   size = thickness * 2 + 1;
   canvas.width = canvas.height = size;
-  drawCross(+range.value)
+  render(+range.value)
 };
 
 find_hex.oninput = e => {
@@ -92,12 +109,20 @@ find_hex.oninput = e => {
     hexNum = hexNum.replace(/./g, a => `${a}${a}`);
   }
   let rgb = Array.from({ length: 3 }, (_, i) => parseInt(hexNum.slice(i * 2, i * 2 + 2), 16));
-  let rgbD = rgb.map(a => a / 255);
 
-  let lch = Lab_to_LCH(XYZ_to_Lab(D65_to_D50(lin_2020_to_XYZ(lin_2020(rgbD)))));
+  // let rgbD = rgb.map(a => a / 255);
+  // let lch = Lab_to_LCH(XYZ_to_Lab(D65_to_D50(lin_2020_to_XYZ(lin_2020(rgbD)))));
+
+  let lch = new Color({ color: rgb, type: 'rgb' }).lchab;
   range.value = lch[0];
+  chroma = lch[1];
+  hue = lch[2];
+  let style = `translate(-50%, -50%) rotate(${-hue + 180}deg) translateY(${-chroma / (MAX_CHROMA / 100)}%)`;
+  cursor.style.transform = style;
+
   // max_chroma.value = lch[1];
   // MAX_CHROMA = +max_chroma.value;
-  drawCross(+range.value);
+
+  render(+range.value);
 };
-find_hex.oninput();
+// find_hex.oninput();
