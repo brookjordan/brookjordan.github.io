ce21ad09840d5dde271438259e7a22a2ccf929ed
Brook Jordan <brook.jordan@motional.com>
2022-09-13 14:53:11 +0800

    add lch support

---
+++ motional/swatches/sorting.js
@@ -0,0 +1,16 @@
+﻿import Color from "./color.js";
+
+const ordered = [...document.querySelectorAll('color-swatch')]
+  .sort((a, b) =>
+    {
+
+      return (new Color(`#${a.hex}`).to('lch').l) - (new Color(`#${b.hex}`).to('lch').l);
+    }
+  )
+  .filter(a => /asphalt|black|white|ambient/i.test(a.name));
+  // .filter(a => {
+  //   const clr = new Color(`#${a.hex}`).to('lch');
+  //   return clr.c < 10 && clr.h > 230 && clr.h < 290;
+  // });
+document.querySelector('.colors').innerHTML = '';
+ordered.forEach(elt => document.querySelector('.colors').append(elt));
