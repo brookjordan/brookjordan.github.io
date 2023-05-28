c925639d00be7d15ada5a0f463e0ebc28952968f
Brook Jordan <brook.jordan@motional.com>
2023-05-28 15:21:04 +0800

    test-web-rtc

---
+++ projects/test-webrtc/get-stun-servers.js
@@ -0,0 +1,80 @@
+let cachedList = JSON.parse(localStorage.getItem("stunList") || "null");
+let asyncStunList = null;
+
+const getPublicStunServerListFromServer = async () => {
+  const stunListPromise =
+    asyncStunList ||
+    fetch(
+      "https://raw.githubusercontent.com/pradt2/always-online-stun/master/valid_hosts.txt"
+    )
+      .then((response) => response.text())
+      .catch(() => {
+        asyncStunList = null;
+        return null;
+      });
+
+  return (await stunListPromise)?.split("\n");
+};
+
+const getPublicStunServerList = async () => {
+  if (
+    cachedList &&
+    Date.now() - new Date(cachedList.dateAdded).getTime() < 10 * 60 * 1000
+  ) {
+    return cachedList.urls;
+  }
+
+  return getPublicStunServerListFromServer().then((urls) => {
+    localStorage.setItem(
+      "stunList",
+      JSON.stringify({
+        dateAdded: new Date(),
+        urls,
+      })
+    );
+    return urls;
+  });
+};
+
+const getRandomStunServer = async () => {
+  let servers = await getPublicStunServerList();
+
+  return servers[Math.floor(Math.random() * servers.length)] || null;
+};
+
+const getStunServers = async ({ searchParam } = {}) => {
+  let cachedStunServers = searchParam
+    ? new URLSearchParams(location.search).get(searchParam)
+    : null;
+
+  if (!cachedStunServers) {
+    const newStunServers = [
+      await getRandomStunServer(),
+      await getRandomStunServer(),
+      await getRandomStunServer(),
+    ];
+
+    if (searchParam) {
+      let encodedServer = btoa(newStunServers);
+      let newURL = new URL(location);
+      let uRLSearch = new URLSearchParams(location.search);
+      uRLSearch.set(searchParam, encodedServer);
+      newURL.search = uRLSearch;
+      history.replaceState(null, "", newURL.toString());
+    }
+
+    return newStunServers;
+  }
+
+  return atob(cachedStunServers).split(",");
+};
+
+getStunServers({ searchParam: "stunServers" }).then((serverUrls) => {
+  if (globalThis.buttoncreateoffer?.style.opacity) {
+    buttoncreateoffer.style.opacity = 1;
+  }
+  if (globalThis.buttonofferpasted?.style.opacity) {
+    buttonofferpasted.style.opacity = 1;
+  }
+  globalThis.stunServerUrls = serverUrls;
+});
