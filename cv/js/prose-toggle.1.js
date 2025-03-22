e66ff917002d967fc812f29a3c21741f60474380
Brook Jordan <brkjrdn@gmail.com>
2025-03-22 20:21:11 +0800

    cv march 2025

---
+++ cv/js/prose-toggle.1.js
@@ -0,0 +1,27 @@
+﻿const prose = ["fluffy", "formal"];
+
+const PROSE_STORAGE_NAME = "CURRENT_PROSE";
+
+const proseInputs = [...prose].map((prose) => ({
+  prose,
+  element: document.querySelector(`#prose--${prose}`),
+}));
+
+const updateProse = (newProse) => {
+  console.log(newProse);
+
+  const selectedInput = proseInputs.find(({ prose }) => prose === newProse);
+  if (!selectedInput) return;
+
+  localStorage.setItem(PROSE_STORAGE_NAME, newProse);
+  proseInputs.forEach(({ element }) => element.removeAttribute("checked"));
+  selectedInput.element.setAttribute("checked", "checked");
+};
+
+updateProse(localStorage.getItem(PROSE_STORAGE_NAME) || "formal");
+
+proseInputs.forEach(({ prose, element }) => {
+  element.addEventListener("change", () => {
+    updateProse(prose);
+  });
+});
