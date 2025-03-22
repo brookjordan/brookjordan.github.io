659c78769c13c2358ac0f83ef574cbeeef57452d
Brook Jordan <brkjrdn@gmail.com>
2025-03-22 20:21:11 +0800

    cv march 2025

--- cv/js/theme-toggle.1.js
+++ cv/js/theme-toggle.1.js
@@ -5,32 +5,39 @@ const themeMetaTags = themes.map((theme) => ({
   element: document.querySelector(`#theme-color-meta-${theme}`),
 }));
 
-const inputs = [...themes, "auto"].map((theme) => ({
-  theme,
+const themeInputs = [...themes, "auto"].map((theme) => ({
+  prose: theme,
   element: document.querySelector(`#theme--${theme}`),
 }));
 
 const THEME_STORAGE_NAME = "CURRENT_THEME";
 
 const updateTheme = (newTheme) => {
+  console.log(newTheme);
   if (newTheme === "auto") {
     localStorage.removeItem(THEME_STORAGE_NAME);
     themeMetaTags.forEach(({ element }) => document.head.append(element));
   } else {
     localStorage.setItem(THEME_STORAGE_NAME, newTheme);
     themeMetaTags.forEach(({ element }) => element.remove());
-    document.head.appendChild(
-      themeMetaTags.find(({ theme }) => theme === newTheme).element
+
+    const newThemeMetaTag = themeMetaTags.find(
+      ({ theme }) => theme === newTheme
     );
+    if (!newThemeMetaTag) return;
+
+    document.head.appendChild(newThemeMetaTag.element);
   }
-  inputs.forEach(({element}) => element.removeAttribute("checked"));
-  inputs.find(({theme}) => theme === newTheme).element.setAttribute("checked", "checked");
+  themeInputs.forEach(({ element }) => element.removeAttribute("checked"));
+  themeInputs
+    .find(({ prose: theme }) => theme === newTheme)
+    .element.setAttribute("checked", "checked");
 };
 
 updateTheme(localStorage.getItem(THEME_STORAGE_NAME) || "auto");
 themeMetaTags.forEach(({ element }) => element.removeAttribute("media"));
 
-inputs.forEach(({ theme, element }) => {
+themeInputs.forEach(({ prose: theme, element }) => {
   element.addEventListener("change", () => {
     updateTheme(theme);
   });
